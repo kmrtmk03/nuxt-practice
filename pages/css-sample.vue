cc34fa44b161d9f8382940eca3b21c0c6f19fca4
kimura <t.kimura@zizo.ne.jp>
2020-05-20 13:08:58 +0900

    ページを追加

---
+++ pages/css-sample.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .container
+    h1 CSS Samples
+    .style-0
+
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 16px;
+    padding: 10px 0;
+  }
+</style>
