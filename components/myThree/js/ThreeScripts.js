197024bd9aaa6b09753bfc495c44b29bf45a2017
kimura <t.kimura@zizo.ne.jp>
2020-06-17 19:43:18 +0900

    add cities

--- components/myThree/js/ThreeScripts.js
+++ components/myThree/js/ThreeScripts.js
@@ -25,9 +25,27 @@ export default class ThreeScripts {
     
     //Canvasの初期化
     this.Init()
+
+
+    this.scrollAmount = 0
+    this.cityPosition = [
+      {x: 0, z: 0},
+
+      {x: 2.5, z: 0},
+      {x: -2.5, z: 0},
+
+      {x: 5, z: 0},
+      {x: -5, z: 0},
+
+      {x: 7.5, z: 0},
+      {x: -7.5, z: 0},
+
+      {x: 10, z: 0},
+      {x: -10, z: 0},
+    ]
   }
 
-  Init() {  
+  Init() { 
 
     //Renderer
     this.renderer = new THREE.WebGLRenderer({
@@ -35,44 +53,73 @@ export default class ThreeScripts {
     })
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(this.windowSize.w, this.windowSize.h)
+    this.renderer.shadowMap.enabled = true
 
     //Scene
     this.scene = new THREE.Scene()
 
     //Camera
     this.camera = new THREE.PerspectiveCamera(45, this.windowSize.w / this.windowSize.h)
-    this.camera.position.set(0, 0, 5)
+    this.camera.position.set(0, 2, 3)
+    this.camera.lookAt(new THREE.Vector3(0, 0.5, 0))
 
     // 平行光源
     this.directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-    this.directionalLight.position.set(1, 1, 1);
-    // シーンに追加
-    this.scene.add(this.directionalLight);
+    this.directionalLight.position.set(0, 10, 5);
+    this.directionalLight.castShadow = true
+    this.scene.add(this.directionalLight);    
     
     //GLTFLoader
     this.loader = new GLTFLoader()
     this.loader.load('../city-0.gltf', data =>  {
       var gltf = data
       this.city = gltf.scene
-
-      this.city.position.y -= 1
-
+      // this.city.position.y -= 1
       this.scene.add(this.city)
 
       //SampleObjectを作成
       this.Render()
     })
+
+    for(let i = 0; i < 9; i++) {
+      //GLTFLoader
+      this.loader = new GLTFLoader()
+      this.loader.load('../city-0.gltf', data =>  {
+        var gltf = data
+        this.city = gltf.scene
+        this.city.position.x -= this.cityPosition[i].x
+        this.city.position.z -= this.cityPosition[i].z
+        this.city.castShadow = true
+        this.city.receiveShadow = true
+        this.scene.add(this.city)
+      })
+    }
+        
+    this.SetEvent()
   }
 
+  SetEvent() {
+    window.addEventListener('mousewheel', e => {
+      e.preventDefault()
+    }, { passive: false })
+    
+    window.onmousewheel = (e) => {
+      this.MoveCamera(e.wheelDelta)
+    }
+  }
+
+  MoveCamera(_scrollValue) {
+    this.scrollAmount += _scrollValue
+    let _value = this.scrollAmount * 0.00002
+    this.camera.position.set(Math.sin(_value) * 10, 2, 3)
+    // this.camera.lookAt(new THREE.Vector3(0, 0, 0))
+  }
 
   //Sample Object
   Render() {
-
     requestAnimationFrame(() => {
       this.Render()
     })
-
-    this.city.rotation.y += 0.01
 
     //Renderer
     this.renderer.render(this.scene, this.camera)
