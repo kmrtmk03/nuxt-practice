112efbe0fd2039146f314054517d7f27bc87e361
Kimura Tomoki <krmmmmmmrk@gmail.com>
2020-06-17 20:13:20 +0900

    add gltf

--- components/myThree/js/ThreeScripts.js
+++ components/myThree/js/ThreeScripts.js
@@ -31,17 +31,17 @@ export default class ThreeScripts {
     this.cityPosition = [
       {x: 0, z: 0},
 
-      {x: 2.5, z: 0},
-      {x: -2.5, z: 0},
+      {x: 4, z: 0},
+      {x: -4, z: 0},
 
-      {x: 5, z: 0},
-      {x: -5, z: 0},
+      {x: 8, z: 0},
+      {x: -8, z: 0},
 
-      {x: 7.5, z: 0},
-      {x: -7.5, z: 0},
+      {x: 12, z: 0},
+      {x: -12, z: 0},
 
-      {x: 10, z: 0},
-      {x: -10, z: 0},
+      {x: 16, z: 0},
+      {x: -16, z: 0},
     ]
   }
 
@@ -49,7 +49,8 @@ export default class ThreeScripts {
 
     //Renderer
     this.renderer = new THREE.WebGLRenderer({
-        canvas: this.canvas
+        canvas: this.canvas,
+        antialias: true
     })
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(this.windowSize.w, this.windowSize.h)
@@ -59,32 +60,30 @@ export default class ThreeScripts {
     this.scene = new THREE.Scene()
 
     //Camera
-    this.camera = new THREE.PerspectiveCamera(45, this.windowSize.w / this.windowSize.h)
-    this.camera.position.set(0, 2, 3)
-    this.camera.lookAt(new THREE.Vector3(0, 0.5, 0))
+    this.camera = new THREE.PerspectiveCamera(50, this.windowSize.w / this.windowSize.h)
+    this.camera.position.set(0, 1.5, 4)
+    this.camera.lookAt(new THREE.Vector3(0, 1.5, 0))
 
     // 平行光源
     this.directionalLight = new THREE.DirectionalLight(0xFFFFFF);
     this.directionalLight.position.set(0, 10, 5);
     this.directionalLight.castShadow = true
-    this.scene.add(this.directionalLight);    
-    
-    //GLTFLoader
-    this.loader = new GLTFLoader()
-    this.loader.load('../city-0.gltf', data =>  {
-      var gltf = data
-      this.city = gltf.scene
-      // this.city.position.y -= 1
-      this.scene.add(this.city)
+    this.scene.add(this.directionalLight);
 
-      //SampleObjectを作成
-      this.Render()
-    })
+    let _directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+    _directionalLight.position.set(0, 0, 10);
+    _directionalLight.castShadow = true
+    this.scene.add(_directionalLight);
+
+    let _directionalLight2 = new THREE.DirectionalLight(0xFFFFFF);
+    _directionalLight2.position.set(0, 0, -10);
+    _directionalLight2.castShadow = true
+    this.scene.add(_directionalLight2);        
 
     for(let i = 0; i < 9; i++) {
       //GLTFLoader
       this.loader = new GLTFLoader()
-      this.loader.load('../city-0.gltf', data =>  {
+      this.loader.load('../base.gltf', data =>  {
         var gltf = data
         this.city = gltf.scene
         this.city.position.x -= this.cityPosition[i].x
@@ -94,6 +93,8 @@ export default class ThreeScripts {
         this.scene.add(this.city)
       })
     }
+    //SampleObjectを作成
+    this.Render()
         
     this.SetEvent()
   }
@@ -111,8 +112,7 @@ export default class ThreeScripts {
   MoveCamera(_scrollValue) {
     this.scrollAmount += _scrollValue
     let _value = this.scrollAmount * 0.00002
-    this.camera.position.set(Math.sin(_value) * 10, 2, 3)
-    // this.camera.lookAt(new THREE.Vector3(0, 0, 0))
+    this.camera.position.set(Math.sin(_value) * 10, 1.5, 4)
   }
 
   //Sample Object
