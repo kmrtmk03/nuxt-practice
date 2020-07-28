65a4566c91a1b87a4bb0cf67f5ea34d13f0caaad
kimura <t.kimura@zizo.ne.jp>
2020-07-28 17:41:04 +0900

    add page

---
+++ pages/p5/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .wrapper
+    h1 p5
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  components: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
